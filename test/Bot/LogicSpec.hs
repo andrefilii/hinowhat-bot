341accce009f4efd87ea8ed6a31515ac7643d848
Andrea Filippi <andrea.filippi02@outlook.it>
2026-01-16 00:36:25 +0100

    test: setup hspec infrastructure

---
+++ test/Bot/LogicSpec.hs
@@ -0,0 +1,10 @@
+module Bot.LogicSpec (spec) where
+
+import Test.Hspec
+
+-- TODO implement real test for the functions in src/Bot/Logic.hs
+spec :: Spec
+spec = do
+  describe "Bot.Logic" $ do
+    it "is waiting to be implemented" $ do
+      pendingWith "Tests not yet written"
