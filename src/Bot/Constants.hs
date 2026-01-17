9991175671c350b518dd4b6db87a49ce4eec4585
Andrea Filippi <andrea.filippi02@outlook.it>
2026-01-17 12:04:03 +0100

    feat: add Constants file for config variables

---
+++ src/Bot/Constants.hs
@@ -0,0 +1,9 @@
+{- |
+    Module      : Bot.Constants
+    Description : Global constants and configuration values
+-}
+module Bot.Constants where
+
+-- | The root directory where the database (JSON files) is located.
+dataDir :: String
+dataDir = "data"
